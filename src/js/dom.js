620052f4a94eea612182672535c116580d94c009
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-09-09 11:51:38 +0200

    Validacion de numero recibido

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,3 +1,17 @@
 /**
  * Responsabilidad: gestionar el los flujos de que recibe y muestra la interfáz
  * */ 
+let obj = [];
+function receivedNumber(value) {
+    if(value % 3 === 0){
+        document.getElementById("result").innerHTML = "El número ingresado es multiplo de 3"
+    }
+    if(value % 5 === 0){
+        document.getElementById("result").innerHTML = "El número ingresado es multiplo de 5"
+    }
+    if(value % 3 === 0 && value % 5 === 0){
+        document.getElementById("result").innerHTML = "El número ingresado es multiplo de 3 y 5"
+    }
+    obj.push(value);
+    console.log(obj)
+}
